789949aeafc20d8d9f02d86ec2fcf1ff1ab6a624
litaiqing <litaiqing@analysys.com.cn>
2019-04-12 18:53:37 +0800

    update

--- brush-assist.js
+++ brush-assist.js
@@ -2,51 +2,72 @@
  * brush-assist.js
  *      brush.js辅助js。
  * Created by litaiqing on 2016-10-27.
- * email : ltqaaa@qq.com (欢迎指正与批评不足。)
+ * email : ohbugs@foxmail.com
  */
-$().ready(function(){
+$().ready(function () {
     window.brush_templ = {};
     $.extend({
-        cut_html : function(html){
-                    return html.replace(/\n+/g,"")
-                               .replace(/<!--.*?-->/ig,"")
-                               .replace(/\/\*.*?\*\//ig,"")
-                               .replace(/[ ]+</ig,"<");
-                    },
-        depot : {
-                    root : 'b-view-',
-                    /**
-                     * 保存一个模版
-                     */
-                    set : function(key, value){
-                        brush_templ[$.depot.root + key] = value;
-                    },
-                    /**
-                     * 读取一个模版
-                     */
-                    get : function(key){
-                        return brush_templ[$.depot.root + key];
-                    },
-                    /**
-                     * 删除一个模版
-                     */
-                    rm : function(key){
-                        try {
-                            delete brush_templ[$.depot.root + key];
-                        } catch (e) {
-                            brush_templ[$.depot.root + key] = null;
-                        }
-                    }
+        cut_html: function (html) {
+            return html.replace(/\n+/g, "")
+                .replace(/<!--.*?-->/ig, "")
+                .replace(/\/\*.*?\*\//ig, "")
+                .replace(/[ ]+</ig, "<")
+                .trim();
+        },
+        depot: {
+            root: 'b-view-',
+            /**
+             * 保存一个模版
+             */
+            set: function (key, value) {
+                brush_templ[$.depot.root + key] = value;
+            },
+            /**
+             * 读取一个模版
+             */
+            get: function (key) {
+                return brush_templ[$.depot.root + key];
+            },
+            /**
+             * 删除一个模版
+             */
+            rm: function (key) {
+                try {
+                    delete brush_templ[$.depot.root + key];
+                } catch (e) {
+                    brush_templ[$.depot.root + key] = null;
                 }
+            }
+        }
+
+    });
+    $.extend($.brush, {
+        // 快速刷新
+        flush: function (name, data, i) {
+            var dataObj = $.depot.get(name);
+            $.brush.paste(dataObj.html, data, {
+                i: i,
+                name: dataObj.alias,
+                callback: function (source, obj, setting) {
+                    $('[b-view="' + name + '"]').html($('[b-view="' + name + '"]').html() + source)
+                    return source;
+                }
+            })
+        }
     });
     /**
      * 加载页面上所有模版
      */
-    $('[b-view]').each(function(){
+    $('[b-view]').each(function () {
         var $view = $(this);
-        var key = $view.attr('b-view');
+        var attr = $view.attr('b-view').split(' as ');
+        attr[1] = attr[1] || 'data';
         var html = $.cut_html($view.html());
-        $.depot.set(key, html);
+        $.depot.set(attr[0], {
+            key: attr[0],
+            alias: attr[1],
+            html: html,
+        });
         $view.empty();
     });
 
--- brush.js
+++ brush.js
@@ -5,18 +5,18 @@
  *      简单（也可以说粗暴）的实现了模版刷子。
  *      适用多种复杂场合
  * Created by litaiqing on 2016-10-27.
- * email : ltqaaa@qq.com (欢迎指正与批评不足。)
+ * email : ohbugs@foxmail.com
  */
-+function($){
++function ($) {
     var brush = {
         /**
          * 内部方法，除去匹配字符串两边的大括号
          * @param text
          * @returns {string}
          */
-        strip : function(text){
+        strip: function (text) {
             var trimLeft = /^\{[^\{]?\{/, trimRight = /\}[^\{]?\}$/;
-            return text.toString().replace( trimLeft, "" ).replace( trimRight, "" );
+            return text.toString().replace(trimLeft, "").replace(trimRight, "");
         },
         /**
          * 格式化批量输出一个模版，并执行模版中的代码
@@ -25,11 +25,14 @@
          * @param length
          * @returns {string}
          */
-        paste_data : function(source, obj, length){
+        paste_data: function (source, obj, setting) {
             var goods = '';
-            for(var i = 0; i < length; i++) {
-                goods += $.brush.format(source, obj, i);
-            };
+            setting = setting || {};
+            var length = setting.length || obj.length || 0;
+            for (var i = 0; i < length; i++) {
+                setting.i = i;
+                goods += $.brush.format(source, obj, setting);
+            }
             return goods;
         },
         /**
@@ -38,12 +41,14 @@
          * @param list
          * @returns {*}
          */
-        paste : function (source, list){
-            if($.type(list) == 'object'){
-                return $.brush.format(source, list);
-            }else if($.type(list) == 'array'){
-                if(list.length == 0){ return ''; } // 如果list不包含元素，则返回'';
-                return $.brush.paste_data(source, list, list.length);
+        paste: function (source, obj, setting) {
+            if ($.type(obj) == 'object') {
+                return $.brush.format(source, obj, setting);
+            } else if ($.type(obj) == 'array') {
+                if (obj.length == 0) {
+                    return '';
+                } // 如果list不包含元素，则返回'';
+                return $.brush.paste_data(source, obj, setting);
             }
             return '';
         },
@@ -54,20 +59,29 @@
          * @param i
          * @returns {*}
          */
-        format : function (source, obj, i) {
-            if($.isEmptyObject(obj)){ return ''; };
+        format: function (source, obj, setting) {
+            if ($.isEmptyObject(obj)) {
+                return '';
+            }
+            setting = setting || {};
+            setting.i = setting.i || 0;
+            setting.index = setting.index || 'i';
+            setting.name = setting.name || 'data';
             var reg = new RegExp('(\{\{).*?(\}\})', 'g');
             var result = source.match(reg);
-            for(var x = 0, l = result.length; x < l; x++){
+            for (var x = 0; x < result.length; x++) {
                 var item = result[x];
                 var code = $.brush.strip(item);
-                source = source.replace(item, function(){
-                    return new Function('data', 'i', 'return ' + code + ' ;')(obj, i);
+                source = source.replace(item, function () {
+                    return new Function(setting.name, setting.index, 'return ' + code + ' ;')(obj, setting.i);
                 });
+            }
+            setting.callback = setting.callback || function () {
             };
+            source = setting.callback(source, obj, setting) || source;
             return source;
         },
-        isBlank : function( text ){
+        isBlank: function (text) {
             if (text == null ||
                 typeof (text) == 'undefined' ||
                 text == undefined) {
@@ -78,5 +92,5 @@
             return false;
         }
     };
-    $.extend({brush : brush});
+    $.extend({brush: brush});
 }($);
